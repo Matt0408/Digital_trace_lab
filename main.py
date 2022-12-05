41aa033e8b34efa2546d73882284651a79cbda10
Matt0408 <matthieu.haution@epfedu.fr>
2022-12-05 15:08:26 +0100

    updating main.py

--- main.py
+++ main.py
@@ -4,4 +4,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def hello_world():
-    return "Hello World"
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=G-SQ85FHJV94"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', ' G-SQ85FHJV94');
+</script>
+ """
+ return prefix_google + "Hello World"
